b8404674a3e877d6c8011fff95d0ba420dc49b43
Zachary Sunberg <sunbergzach@gmail.com>
2017-05-04 11:06:31 -0700

    added mcts test

---
+++ test/run_mcts.jl
@@ -0,0 +1,8 @@
+p5 = LowLevelMDP()
+solver = MCTSSolver()
+policy = solve(solver, p5)
+for i in 1:10
+  print("\r$i")
+  hr = HistoryRecorder(max_steps = 20)
+  simulate(hr, p5, policy)
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,21 @@
 using HierarchicalDriving
 using Base.Test
 
-using MCVI
 using Base.Test
-import MCVI: init_lower_action, lower_bound, upper_bound
 using POMDPs
 using GenerativeModels
+
+using MCTS
+# using MCVI
+# import MCVI: init_lower_action, lower_bound, upper_bound
 
 using POMDPModels # for LightDark1d
 
 # write your own tests here
 #@test 1 == 2
 include("run_random.jl")
+
+include("run_mcts.jl")
 
 #include("runMCVI.jl")
 

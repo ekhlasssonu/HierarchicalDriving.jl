9af7059f3fbca1024fc941065c995958d66d3942
Zachary Sunberg <sunbergzach@gmail.com>
2017-05-04 14:15:09 -0700

    road visualization based on mdp, better everything

--- src/Visualization.jl
+++ src/Visualization.jl
@@ -8,28 +8,43 @@ using Iterators
 
 import Base.show
 
+function show(io::IO, mime::MIME"image/png", t::Tuple{LowLevelMDP, GlobalStateL1})
+    mdp = t[1]
+    s = t[2]
+    scene = convert(Scene, s)
+    n_lanes = length(mdp.nbrLaneMarkings)-1
+
+    es = s.ego.state
+    # origin is center of rightmost lane
+    roadway = gen_straight_roadway(n_lanes,
+                                   es[1]+1000.0,
+                                   lane_widths=diff(mdp.nbrLaneMarkings),
+                                   origin=VecSE2(es[1]-500.0, mean(mdp.nbrLaneMarkings[1:2]), 0.0),
+                                  )
+    # cam = FitToContentCamera(0.1)
+    cam = SceneFollowCamera(12.0) # second number is pixels per meter
+    c = render(scene, roadway, [CarVelOverlay(), CarIDOverlay()], cam=cam)
+    show(io, mime, c)
+end
+
 function show(io::IO, mime::MIME"image/png", s::GlobalStateL1)
+    scene = convert(Scene, s)
+    cam = SceneFollowCamera(12.0) # second number is pixels per meter
+    c = render(scene, nothing, [CarVelOverlay(), CarIDOverlay()], cam=cam)
+    show(io, mime, c)
+end
+
+function convert(::Type{Scene}, s::GlobalStateL1)
     scene = Scene()
     es = s.ego.state
-    xmax = es[1]
     push!(scene, Vehicle(VehicleState(VecSE2(es[1], es[2], 0.0), es[3]),
                          VehicleDef(), 0))
     for (i,cs) in enumerate(chain(s.neighborhood...))
         s = cs.physicalState.state
         push!(scene, Vehicle(VehicleState(VecSE2(s[1], s[2], 0.0), s[3]),
                              VehicleDef(), i))
-        xmax = max(xmax, s[1])
     end
-    n_lanes = 40 # must be even
-    roadway = gen_straight_roadway(n_lanes,
-                                   xmax+1000.0,
-                                   lane_width=LANE_WIDTH,
-                                   origin=VecSE2(-500.0, -n_lanes/2*LANE_WIDTH, 0.0),
-                                  )
-    # cam = FitToContentCamera(0.1)
-    cam = SceneFollowCamera(12.0) # second number is pixels per meter
-    c = render(scene, roadway, [CarVelOverlay(), CarIDOverlay()], cam=cam)
-    show(io, mime, c)
+    return scene
 end
 
 type CarVelOverlay <: SceneOverlay end
@@ -38,8 +53,8 @@ function AutoViz.render!(rm::RenderModel, o::CarVelOverlay, scene::Scene, roadwa
     for (i,v) in enumerate(scene)
         cx = v.state.posG.x
         cy = v.state.posG.y
-        vx = cx + v.def.length/2 - 1.4
-        vy = cy + v.def.width/2 - 0.4
+        vx = cx + v.def.length/2 + 1.0
+        vy = cy + v.def.width/2 - 0.5
         add_instruction!(rm, render_text,
                          (@sprintf("%04.1f",v.state.v), vx, vy, 14, colorant"white"))
     end
@@ -55,8 +70,8 @@ function AutoViz.render!(rm::RenderModel, o::CarIDOverlay, scene::Scene, roadway
         # ny = cy + v.def.width/2 - 0.6
         # add_instruction!(rm, render_text,
         #                  (@sprintf("%02.d",i), nx, ny, 14, colorant"white"))
-        idx = cx - v.def.length/2
-        idy = cy - v.def.width/2 + 0.1
+        idx = cx + v.def.length/2 + 1.0
+        idy = cy - v.def.width/2
         add_instruction!(rm, render_text,
                          (@sprintf("%02.d",v.id), idx, idy, 14, colorant"white"))
     end
